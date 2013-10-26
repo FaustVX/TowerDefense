9aba3a4bf1156edd81cfec3b2dff7d1c0a2f7b2a
FaustVX <jodu035@gmail.com>
2013-10-26 12:39:19 +0200

    Ajout du server

---
+++ TowerDefense.Server/Main.cs
@@ -0,0 +1,39 @@
+using System;
+using CSharpHelper;
+
+namespace TowerDefense.Server
+{
+	public class Cell:Cell<Cell>
+	{
+		public Cell (int x, int y, Grid<Cell> list):
+			base(x, y, list, false)
+		{
+			
+		}
+	}
+
+	public class Grid:Grid<Cell>
+	{
+		public Grid (int x, int y)
+			: base(x, y, CreateCell)
+		{
+			
+		}
+			
+		private Cell CreateCell(int x, int y, Grid<Cell> list)
+		{
+			return new Cell(x, y, list);
+		}
+	}
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Grid grid=new Grid(5, 5);
+
+			Console.WriteLine ("Hello World!");
+			Console.Read ();
+		}
+	}
+}
